a73233fa3e959ce0a10e9ff886aded769a5d9b8e
Glacialan <44652980+Glacialan@users.noreply.github.com>
2018-12-05 13:15:06 -0500

    Update clinton.py

--- clinton.py
+++ clinton.py
@@ -1,1 +1,28 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
+import asyncio
+from itertools import cycle
+import random
+import time
+import os
 
+Client = discord.Client()
+client = commands.Bot(command_prefix = ["k!", "K!"])
+client.remove_command("help")
+# - - - - -
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name="Hentai Only For The Plot", type=3))
+    print("Bot Status: Online")
+    print(" ")
+    print("Logged in as: " + client.user.name)
+    print(" ")
+    print("- - - - -")
+
+# - - - - -
+
+
+
+client.run(os.getenv('TOKEN'))
